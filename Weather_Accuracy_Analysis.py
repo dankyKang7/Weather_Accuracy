caba09b14320d44d8f0eaff46e676df51fc73ae1
Travis Mattson <travmatt7@gmail.com>
2019-11-17 08:43:33 -0600

    Initial commit

---
+++ Weather_Accuracy_Analysis.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 17 08:39:09 2019
+
+@author: admin
+"""
+
+#WEather_Analysis File
+
+import re
+import pandas as pd
+from datetime import datetime
